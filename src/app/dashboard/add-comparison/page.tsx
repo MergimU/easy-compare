f2cdd06f8510b1e0d83ec855fe3fe3a971089142
Mergim Ujkani <mergim.ujkani@gmail.com>
2024-06-03 17:34:07 +0200

    Added "Add" button card in dashboard

---
+++ src/app/dashboard/add-comparison/page.tsx
@@ -0,0 +1,7 @@
+export default function Add() {
+  return (
+    <div className="prose">
+      <h1>Add page1</h1>
+    </div>
+  )
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -12,8 +12,8 @@ export default async function Dashboard() {
   }
   return (
     <div className="w-screen flex items-center flex-col p-8">
-      <div className="grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3">
-        <div className="card bg-base-100 shadow-xl prose min-w-80">
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+        <div className="card bg-base-100 shadow-2xl prose min-w-80">
           <div className="card-body">
             <h2 className="card-title">Project 1</h2>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore, exercitationem!</p>
@@ -22,12 +22,21 @@ export default async function Dashboard() {
             </div>
           </div>
         </div>
-        <div className="card bg-base-100 shadow-xl prose min-w-80">
+        <div className="card bg-base-100 shadow-2xl prose min-w-80">
           <div className="card-body">
             <h2 className="card-title">Project 2</h2>
             <p>Lorem ipsum dolor sit amet consectetur.</p>
             <div className="card-actions justify-end">
               <Link href="/dashboard/project-2" className="btn btn-primary text-white w-full">Show comparison</Link>
+            </div>
+          </div>
+        </div>
+        <div className="card bg-base-100 shadow-2xl prose min-w-80">
+          <div className="card-body text-center">
+            <h2 className="card-title inline">Add new</h2>
+            <p>Click here to start adding a new comparison.</p>
+            <div className="card-actions justify-end">
+              <Link href="/dashboard/add-comparison" className="btn w-full">Add +</Link>
             </div>
           </div>
         </div>
