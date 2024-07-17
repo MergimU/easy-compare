a9451139995a48b577719f1b025d069d86e30e67
Mergim Ujkani <mergim.ujkani@gmail.com>
2024-07-17 16:51:00 +0200

    Fixed cookes getting called outside the function

--- src/app/actions/user.ts
+++ src/app/actions/user.ts
@@ -1,9 +1,9 @@
 'use server';
 
 import { createClient } from '@app/utils/supabase/server'
-const supabase = createClient()
 
 export const userSession = async () => {
+  const supabase = createClient()
   const { data: { user }, error } = await supabase.auth.getUser()
 
   if (error) {
